99fc6ab2f6e2abafabebb0e078663be3b5cfe46d
DuffyWang926 <wef695944018@163.com>
2022-01-07 16:33:50 +0800

    index

--- src/components/ImageCom/index.jsx
+++ src/components/ImageCom/index.jsx
@@ -2,21 +2,46 @@ import Taro from "@tarojs/taro";
 import { View, Image } from "@tarojs/components";
 
 import "./index.scss";
-
+const reMenImgA = require("../../assets/product/1.jpg")
+const reMenImgB = require("../../assets/product/2.jpg")
+const reMenImgC = require("../../assets/product/3.jpg")
+const zuiXinImgA = require("../../assets/product/4.jpg")
+const zuiXinImgB = require("../../assets/product/5.jpg")
+const zuiXinImgC = require("../../assets/product/6.jpg")
 
 const ImageCom = ({props}) => {
   const { id, imgUrl  } = props
+  debugger
 
   const onImgClick = () =>{
     Taro.navigateTo({
       url:`/pages/product/index?id=${id}`
     });
   }
+  let urlEnd = ''
+  if(imgUrl == 1){
+    urlEnd = reMenImgA
+  }else if(imgUrl == 2){
+    urlEnd = reMenImgB
+  }else if(imgUrl == 3){
+    urlEnd = reMenImgC
+  }else if(imgUrl == 4){
+    urlEnd = zuiXinImgA
+  }else if(imgUrl == 5){
+    urlEnd = zuiXinImgB
+  }else if(imgUrl == 6){
+    urlEnd = zuiXinImgC
+  }else{
+    urlEnd = imgUrl
+  }
+
+
 
   return (
     <View className="imageCom" >
       <Image
-       src={imgUrl}
+       src={urlEnd}
+       key={urlEnd}
        onClick = { () => {onImgClick()}}
        className="imageComImg"
       ></Image>
