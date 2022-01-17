abef7f1e9f6bf59043fa58db2867a34606be6344
DuffyWang926 <wef695944018@163.com>
2022-01-17 16:33:04 +0800

    change home

--- controllers/home.js
+++ controllers/home.js
@@ -1,16 +1,68 @@
-const homeData = async (ctx, next) => {
-    var
-        name = ctx.request.body.name || '',
-        password = ctx.request.body.password || '';
-    console.log(`signin with name: ${name}, password: ${password}`);
-    if (name === 'koa' && password === '12345') {
-        ctx.response.body = `<h1>Welcome, ${name}!</h1>`;
-    } else {
-        ctx.response.body = `<h1>Login failed!</h1>
-        <p><a href="/">Try again</a></p>`;
+const model = require('../model');
+const _ = require('lodash')
+
+const fn_banner = async (ctx, next) => {
+    let productModel = model.user
+
+    let users = await  productModel.findAll({
+        where: {
+        }
+    })
+    console.log(`find ${users} users:`);
+    
+    ctx.response.body = {
+        bannerList:users
     }
 };
 
+const fn_home = async (ctx, next) => {
+    let productModel = model.product
+
+    let products = await  productModel.findAll({
+        where: {
+        }
+    })
+    console.log(`find ${products} products:`);
+    let imgList = Array.isArray(products) && products.map( (v,i) =>{
+        let res = {
+            imgId:v.productId,
+            imgUrl:v.imgUrl,
+            title:v.title,
+            author:v.author,
+            downSum:v.sum,
+            updatedAt:v.updatedAt
+        }
+        return res
+    })
+    imgList.sort((a,b) =>{
+        return a.downSum - b.downSum
+    })
+    let imgListEnd = imgList.slice(0,3)
+    let nextList = _.cloneDeep(imgList)
+    nextList.sort((a,b) =>{
+        return b.updatedAt - a.updatedAt
+    })
+    let newList = nextList.slice(0,3)
+    console.log(imgList)
+    console.log(nextList)
+    let dataRes = [
+        {
+            title:'热门',
+            type:0,
+            imgList:imgListEnd
+        },
+        {
+            title:'最新',
+            type:1,
+            imgList:newList
+        },
+    ]
+    ctx.response.body = dataRes
+    
+};
+
 module.exports = {
-    'POST /api/home': homeData,
+    'GET /api/home': fn_home,
+    'GET /api/banner': fn_banner,
+
 };
--- controllers/index.js
+++ controllers/index.js
@@ -1,65 +1,3 @@
-const model = require('../model');
-const _ = require('lodash')
-
-const fn_banner = async (ctx, next) => {
-    let productModel = model.user
-
-    let users = await  productModel.findAll({
-        where: {
-        }
-    })
-    console.log(`find ${users} users:`);
-    
-    ctx.response.body = {
-        bannerList:users
-    }
-};
-
-const fn_home = async (ctx, next) => {
-    let productModel = model.product
-
-    let products = await  productModel.findAll({
-        where: {
-        }
-    })
-    console.log(`find ${products} products:`);
-    let imgList = Array.isArray(products) && products.map( (v,i) =>{
-        let res = {
-            imgId:v.productId,
-            imgUrl:v.imgUrl,
-            title:v.title,
-            author:v.author,
-            downSum:v.sum,
-            updatedAt:v.updatedAt
-        }
-        return res
-    })
-    imgList.sort((a,b) =>{
-        return a.downSum - b.downSum
-    })
-    let imgListEnd = imgList.slice(0,3)
-    let nextList = _.cloneDeep(imgList)
-    nextList.sort((a,b) =>{
-        return b.updatedAt - a.updatedAt
-    })
-    let newList = nextList.slice(0,3)
-    console.log(imgList)
-    console.log(nextList)
-    let dataRes = [
-        {
-            title:'热门',
-            type:0,
-            imgList:imgListEnd
-        },
-        {
-            title:'最新',
-            type:1,
-            imgList:newList
-        },
-    ]
-    ctx.response.body = dataRes
-    
-};
 
 
 
@@ -79,9 +17,5 @@ var fn_signin = async (ctx, next) => {
 
 
 module.exports = {
-    // 'GET /api': fn_home,
-    'GET /api/home': fn_home,
-    'GET /api/banner': fn_banner,
     'POST /api/signin': fn_signin,
-    // 'GET /product': fn_home
 };
