3b039ecb97832c9a6346f53eaa08c136e29823b6
DuffyWang926 <wef695944018@163.com>
2022-04-02 21:37:06 +0800

    init

---
+++ src/app.js
@@ -0,0 +1,33 @@
+import { Component } from 'react'
+import { Provider } from 'react-redux'
+import './app.scss'
+
+import configStore from "./store";
+
+const store = configStore();
+
+// class App extends Component {
+
+//   componentDidMount () {}
+
+//   componentDidShow () {}
+
+//   componentDidHide () {}
+
+//   componentDidCatchError () {}
+
+//   render () {
+//     return this.props.children
+//   }
+// }
+
+const App = ({ children }) =>{
+  return (
+    <Provider store={store}>
+      {children}
+    </Provider>
+  );
+  
+}
+
+export default App
